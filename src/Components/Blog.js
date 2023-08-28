199308d600469988e9cd2a37e29902410f37f93d
Deepesh Sharma <deep92112018@gmail.com>
2023-08-28 11:45:05 +0530

    Passing an object in useState()

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,15 +3,15 @@ import { useState } from "react";
 
 export default function Blog(){
 
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+    const [formData, setFormData] = useState({title: "", content: ""});
     const [blogs, setBlogs] = useState([]);
     
     //Passing the synthetic event as argument to stop refreshing the page on submit
     function handleSubmit(e){
         e.preventDefault();
 
-        setBlogs([{title, content}, ...blogs]);
+        setBlogs([{title : formData.title, content : formData.content}, ...blogs]);
+        setFormData({title: "", content: ""});
         console.log(blogs);
     }
 
@@ -29,21 +29,21 @@ export default function Blog(){
                 {/* Row component to create a row for first input field */}
                 <Row label="Title">
                     <input type="text" 
-                            class="form-control" 
+                            className="form-control" 
                             aria-label="Sizing example input" 
                             aria-describedby="inputGroup-sizing-lg"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}        
+                            value={formData.title}
+                            onChange={(e) => setFormData({title: e.target.value, content: formData.content})}        
                     />
                 </Row >
 
                 {/* Row component to create a row for Text area field */}
                 <Row label="Content">
                     <textarea 
-                            class="form-control" 
+                            className="form-control" 
                             aria-label="With textarea"
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
+                            value={formData.content}
+                            onChange={(e) => setFormData({title: formData.title, content: e.target.value})}
                     />
                     
                 </Row >
@@ -60,15 +60,14 @@ export default function Blog(){
 
         <div id="submitted-blogs">
             <h2> Blogs </h2>
-            <p>
+         
                 {blogs.map((blog, index) => (
                     <div className="blog" key={index}>
                         <h3>{blog.title}</h3>
                         <p>{blog.content}</p>
                     </div>
                 ))}
-
-            </p>
+                
         </div>
         </div>
         )
